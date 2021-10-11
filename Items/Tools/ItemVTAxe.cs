469c87fb00b565f4a102a20cb7a429d6973da60a
VinterNacht <crannnabeatha@gmail.com>
2021-10-10 19:51:22 -0500

    Why No Worky?

--- Items/Tools/ItemVTAxe.cs
+++ Items/Tools/ItemVTAxe.cs
@@ -53,176 +53,174 @@ namespace VinterTweaks.Items.Tools
             dustParticles.WindAffectednes = 0.5f;
         }
 
+        public override float OnBlockBreaking(IPlayer player, BlockSelection blockSel, ItemSlot itemslot, float remainingResistance, float dt, int counter)
+        {
+            ITreeAttribute tempAttr = itemslot.Itemstack.TempAttributes;
+            int posx = tempAttr.GetInt("lastposX", -1);
+            int posy = tempAttr.GetInt("lastposY", -1);
+            int posz = tempAttr.GetInt("lastposZ", -1);
+            float treeResistance = tempAttr.GetFloat("treeResistance", 1);
+
+            BlockPos pos = blockSel.Position;
+
+            if (pos.X != posx || pos.Y != posy || pos.Z != posz || counter % 30 == 0)
+            {
+                Stack<BlockPos> foundPositions = FindTree(player.Entity.World, pos);
+                treeResistance = (float)Math.Max(1, Math.Sqrt(foundPositions.Count));
+
+                tempAttr.SetFloat("treeResistance", treeResistance);
+            }
+
+            tempAttr.SetInt("lastposX", pos.X);
+            tempAttr.SetInt("lastposY", pos.Y);
+            tempAttr.SetInt("lastposZ", pos.Z);
 
 
-        //public override float OnBlockBreaking(IPlayer player, BlockSelection blockSel, ItemSlot itemslot, float remainingResistance, float dt, int counter)
-        //{
-        //    ITreeAttribute tempAttr = itemslot.Itemstack.TempAttributes;
-        //    int posx = tempAttr.GetInt("lastposX", -1);
-        //    int posy = tempAttr.GetInt("lastposY", -1);
-        //    int posz = tempAttr.GetInt("lastposZ", -1);
-        //    float treeResistance = tempAttr.GetFloat("treeResistance", 1);
+            return base.OnBlockBreaking(player, blockSel, itemslot, remainingResistance, dt / treeResistance, counter);
+        }
 
-        //    BlockPos pos = blockSel.Position;
+        public override bool OnBlockBrokenWith(IWorldAccessor world, Entity byEntity, ItemSlot itemslot, BlockSelection blockSel, float dropQuantityMultiplier = 1)
+        {
+            IPlayer byPlayer = null;
+            if (byEntity is EntityPlayer) byPlayer = byEntity.World.PlayerByUid(((EntityPlayer)byEntity).PlayerUID);
 
-        //    if (pos.X != posx || pos.Y != posy || pos.Z != posz || counter % 30 == 0)
-        //    {
-        //        Stack<BlockPos> foundPositions = FindTree(player.Entity.World, pos);
-        //        treeResistance = (float)Math.Max(1, Math.Sqrt(foundPositions.Count));
+            double windspeed = api.ModLoader.GetModSystem<WeatherSystemBase>()?.WeatherDataSlowAccess.GetWindSpeed(byEntity.SidedPos.XYZ) ?? 0;
 
-        //        tempAttr.SetFloat("treeResistance", treeResistance);
-        //    }
+            Stack<BlockPos> foundPositions = FindTree(world, blockSel.Position);
 
-        //    tempAttr.SetInt("lastposX", pos.X);
-        //    tempAttr.SetInt("lastposY", pos.Y);
-        //    tempAttr.SetInt("lastposZ", pos.Z);
+            if (foundPositions.Count == 0)
+            {
+                return base.OnBlockBrokenWith(world, byEntity, itemslot, blockSel, dropQuantityMultiplier);
+            }
 
+            bool damageable = DamagedBy != null && DamagedBy.Contains(EnumItemDamageSource.BlockBreaking);
 
-        //    return base.OnBlockBreaking(player, blockSel, itemslot, remainingResistance, dt / treeResistance, counter);
-        //}
+            float leavesMul = 1;
+            float leavesBranchyMul = 0.8f;
+            int blocksbroken = 0;
 
-        //public override bool OnBlockBrokenWith(IWorldAccessor world, Entity byEntity, ItemSlot itemslot, BlockSelection blockSel, float dropQuantityMultiplier = 1)
-        //{
-        //    IPlayer byPlayer = null;
-        //    if (byEntity is EntityPlayer) byPlayer = byEntity.World.PlayerByUid(((EntityPlayer)byEntity).PlayerUID);
+            while (foundPositions.Count > 0)
+            {
+                BlockPos pos = foundPositions.Pop();
+                blocksbroken++;
 
-        //    double windspeed = api.ModLoader.GetModSystem<WeatherSystemBase>()?.WeatherDataSlowAccess.GetWindSpeed(byEntity.SidedPos.XYZ) ?? 0;
+                Block block = world.BlockAccessor.GetBlock(pos);
 
-        //    Stack<BlockPos> foundPositions = FindTree(world, blockSel.Position);
+                bool isLog = block.BlockMaterial == EnumBlockMaterial.Wood;
+                bool isBranchy = block.Code.Path.Contains("branchy");
+                bool isLeaves = block.BlockMaterial == EnumBlockMaterial.Leaves;
 
-        //    if (foundPositions.Count == 0)
-        //    {
-        //        return base.OnBlockBrokenWith(world, byEntity, itemslot, blockSel, dropQuantityMultiplier);
-        //    }
+                world.BlockAccessor.BreakBlock(pos, byPlayer, isLeaves ? leavesMul : (isBranchy ? leavesBranchyMul : 1));
 
-        //    bool damageable = DamagedBy != null && DamagedBy.Contains(EnumItemDamageSource.BlockBreaking);
+                if (world.Side == EnumAppSide.Client)
+                {
+                    dustParticles.Color = block.GetRandomColor(world.Api as ICoreClientAPI, pos, BlockFacing.UP);
+                    dustParticles.Color |= 255 << 24;
+                    dustParticles.MinPos.Set(pos.X, pos.Y, pos.Z);
 
-        //    float leavesMul = 1;
-        //    float leavesBranchyMul = 0.8f;
-        //    int blocksbroken = 0;
+                    if (block.BlockMaterial == EnumBlockMaterial.Leaves)
+                    {
+                        dustParticles.GravityEffect = (float)world.Rand.NextDouble() * 0.1f + 0.01f;
+                        dustParticles.ParticleModel = EnumParticleModel.Quad;
+                        dustParticles.MinVelocity.Set(-0.4f + 4 * (float)windspeed, -0.4f, -0.4f);
+                        dustParticles.AddVelocity.Set(0.8f + 4 * (float)windspeed, 1.2f, 0.8f);
 
-        //    while (foundPositions.Count > 0)
-        //    {
-        //        BlockPos pos = foundPositions.Pop();
-        //        blocksbroken++;
-
-        //        Block block = world.BlockAccessor.GetBlock(pos);
-
-        //        bool isLog = block.BlockMaterial == EnumBlockMaterial.Wood;
-        //        bool isBranchy = block.Code.Path.Contains("branchy");
-        //        bool isLeaves = block.BlockMaterial == EnumBlockMaterial.Leaves;
-
-        //        world.BlockAccessor.BreakBlock(pos, byPlayer, isLeaves ? leavesMul : (isBranchy ? leavesBranchyMul : 1));
-
-        //        if (world.Side == EnumAppSide.Client)
-        //        {
-        //            dustParticles.Color = block.GetRandomColor(world.Api as ICoreClientAPI, pos, BlockFacing.UP);
-        //            dustParticles.Color |= 255 << 24;
-        //            dustParticles.MinPos.Set(pos.X, pos.Y, pos.Z);
-
-        //            if (block.BlockMaterial == EnumBlockMaterial.Leaves)
-        //            {
-        //                dustParticles.GravityEffect = (float)world.Rand.NextDouble() * 0.1f + 0.01f;
-        //                dustParticles.ParticleModel = EnumParticleModel.Quad;
-        //                dustParticles.MinVelocity.Set(-0.4f + 4 * (float)windspeed, -0.4f, -0.4f);
-        //                dustParticles.AddVelocity.Set(0.8f + 4 * (float)windspeed, 1.2f, 0.8f);
-
-        //            }
-        //            else
-        //            {
-        //                dustParticles.GravityEffect = 0.8f;
-        //                dustParticles.ParticleModel = EnumParticleModel.Cube;
-        //                dustParticles.MinVelocity.Set(-0.4f + (float)windspeed, -0.4f, -0.4f);
-        //                dustParticles.AddVelocity.Set(0.8f + (float)windspeed, 1.2f, 0.8f);
-        //            }
+                    }
+                    else
+                    {
+                        dustParticles.GravityEffect = 0.8f;
+                        dustParticles.ParticleModel = EnumParticleModel.Cube;
+                        dustParticles.MinVelocity.Set(-0.4f + (float)windspeed, -0.4f, -0.4f);
+                        dustParticles.AddVelocity.Set(0.8f + (float)windspeed, 1.2f, 0.8f);
+                    }
 
 
-        //            world.SpawnParticles(dustParticles);
-        //        }
+                    world.SpawnParticles(dustParticles);
+                }
 
 
-        //        if (damageable && isLog)
-        //        {
-        //            DamageItem(world, byEntity, itemslot);
-        //        }
+                if (damageable && isLog)
+                {
+                    DamageItem(world, byEntity, itemslot);
+                }
 
-        //        if (itemslot.Itemstack == null) return true;
+                if (itemslot.Itemstack == null) return true;
 
-        //        if (isLeaves && leavesMul > 0.03f) leavesMul *= 0.85f;
-        //        if (isBranchy && leavesBranchyMul > 0.015f) leavesBranchyMul *= 0.6f;
-        //    }
+                if (isLeaves && leavesMul > 0.03f) leavesMul *= 0.85f;
+                if (isBranchy && leavesBranchyMul > 0.015f) leavesBranchyMul *= 0.6f;
+            }
 
-        //    if (blocksbroken > 35)
-        //    {
-        //        Vec3d pos = blockSel.Position.ToVec3d().Add(0.5, 0.5, 0.5);
-        //        api.World.PlaySoundAt(new AssetLocation("sounds/effect/treefell"), pos.X, pos.Y, pos.Z, byPlayer, false, 32, GameMath.Clamp(blocksbroken / 100f, 0.25f, 1));
-        //    }
+            if (blocksbroken > 35)
+            {
+                Vec3d pos = blockSel.Position.ToVec3d().Add(0.5, 0.5, 0.5);
+                api.World.PlaySoundAt(new AssetLocation("sounds/effect/treefell"), pos.X, pos.Y, pos.Z, byPlayer, false, 32, GameMath.Clamp(blocksbroken / 100f, 0.25f, 1));
+            }
 
-        //    return true;
-        //}
+            return true;
+        }
 
-        //public Stack<BlockPos> FindTree(IWorldAccessor world, BlockPos startPos)
-        //{
-        //    Queue<Vec4i> queue = new Queue<Vec4i>();
-        //    HashSet<BlockPos> checkedPositions = new HashSet<BlockPos>();
-        //    Stack<BlockPos> foundPositions = new Stack<BlockPos>();
+        public Stack<BlockPos> FindTree(IWorldAccessor world, BlockPos startPos)
+        {
+            Queue<Vec4i> queue = new Queue<Vec4i>();
+            HashSet<BlockPos> checkedPositions = new HashSet<BlockPos>();
+            Stack<BlockPos> foundPositions = new Stack<BlockPos>();
 
-        //    Block block = world.BlockAccessor.GetBlock(startPos);
-        //    if (block.Code == null) return foundPositions;
+            Block block = world.BlockAccessor.GetBlock(startPos);
+            if (block.Code == null) return foundPositions;
 
-        //    string treeFellingGroupCode = block.Attributes?["treeFellingGroupCode"].AsString();
-        //    int spreadIndex = block.Attributes?["treeFellingGroupSpreadIndex"].AsInt(0) ?? 0;
+            string treeFellingGroupCode = block.Attributes?["treeFellingGroupCode"].AsString();
+            int spreadIndex = block.Attributes?["treeFellingGroupSpreadIndex"].AsInt(0) ?? 0;
 
-        //    // Must start with a log
-        //    if (spreadIndex < 2) return foundPositions;
-        //    if (treeFellingGroupCode == null) return foundPositions;
+            // Must start with a log
+            if (spreadIndex < 2) return foundPositions;
+            if (treeFellingGroupCode == null) return foundPositions;
 
-        //    queue.Enqueue(new Vec4i(startPos.X, startPos.Y, startPos.Z, spreadIndex));
-        //    foundPositions.Push(startPos);
-        //    checkedPositions.Add(startPos);
+            queue.Enqueue(new Vec4i(startPos.X, startPos.Y, startPos.Z, spreadIndex));
+            foundPositions.Push(startPos);
+            checkedPositions.Add(startPos);
 
-        //    while (queue.Count > 0)
-        //    {
-        //        if (foundPositions.Count > 2500)
-        //        {
-        //            break;
-        //        }
+            while (queue.Count > 0)
+            {
+                if (foundPositions.Count > 2500)
+                {
+                    break;
+                }
 
-        //        Vec4i pos = queue.Dequeue();
+                Vec4i pos = queue.Dequeue();
 
-        //        for (int i = 0; i < Vec3i.DirectAndIndirectNeighbours.Length; i++)
-        //        {
-        //            Vec3i facing = Vec3i.DirectAndIndirectNeighbours[i];
-        //            BlockPos neibPos = new BlockPos(pos.X + facing.X, pos.Y + facing.Y, pos.Z + facing.Z);
+                for (int i = 0; i < Vec3i.DirectAndIndirectNeighbours.Length; i++)
+                {
+                    Vec3i facing = Vec3i.DirectAndIndirectNeighbours[i];
+                    BlockPos neibPos = new BlockPos(pos.X + facing.X, pos.Y + facing.Y, pos.Z + facing.Z);
 
-        //            float hordist = GameMath.Sqrt(neibPos.HorDistanceSqTo(startPos.X, startPos.Z));
-        //            float vertdist = (neibPos.Y - startPos.Y);
+                    float hordist = GameMath.Sqrt(neibPos.HorDistanceSqTo(startPos.X, startPos.Z));
+                    float vertdist = (neibPos.Y - startPos.Y);
 
-        //            // "only breaks blocks inside an upside down square base pyramid"
-        //            if (hordist - 1 >= 2 * vertdist) continue;
-        //            if (checkedPositions.Contains(neibPos)) continue;
+                    // "only breaks blocks inside an upside down square base pyramid"
+                    if (hordist - 1 >= 2 * vertdist) continue;
+                    if (checkedPositions.Contains(neibPos)) continue;
 
-        //            block = world.BlockAccessor.GetBlock(neibPos);
-        //            if (block.Code == null || block.Id == 0) continue;
+                    block = world.BlockAccessor.GetBlock(neibPos);
+                    if (block.Code == null || block.Id == 0) continue;
 
-        //            string ngcode = block.Attributes?["treeFellingGroupCode"].AsString();
+                    string ngcode = block.Attributes?["treeFellingGroupCode"].AsString();
 
-        //            // Only break the same type tree blocks
-        //            if (ngcode != treeFellingGroupCode) continue;
+                    // Only break the same type tree blocks
+                    if (ngcode != treeFellingGroupCode) continue;
 
-        //            // Only spread from "high to low". i.e. spread from log to leaves, but not from leaves to logs
-        //            int nspreadIndex = block.Attributes?["treeFellingGroupSpreadIndex"].AsInt(0) ?? 0;
-        //            if (pos.W < nspreadIndex) continue;
+                    // Only spread from "high to low". i.e. spread from log to leaves, but not from leaves to logs
+                    int nspreadIndex = block.Attributes?["treeFellingGroupSpreadIndex"].AsInt(0) ?? 0;
+                    if (pos.W < nspreadIndex) continue;
 
-        //            foundPositions.Push(neibPos.Copy());
-        //            queue.Enqueue(new Vec4i(neibPos.X, neibPos.Y, neibPos.Z, nspreadIndex));
+                    foundPositions.Push(neibPos.Copy());
+                    queue.Enqueue(new Vec4i(neibPos.X, neibPos.Y, neibPos.Z, nspreadIndex));
 
 
-        //            checkedPositions.Add(neibPos);
-        //        }
-        //    }
-        //    return foundPositions;
-        //}
+                    checkedPositions.Add(neibPos);
+                }
+            }
+            return foundPositions;
+        }
 
         public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
@@ -237,13 +235,7 @@ namespace VinterTweaks.Items.Tools
             if (interactedBlock.FirstCodePart() == "log" && interactedBlock.Variant["type"] == "placed")
             {
 
-                //choppingTime = api.World.Config.GetDouble("BaseBarkStrippingSpeed", 1.0) * this.Attributes["strippingTimeModifier"].AsDouble();
-
                 byEntity.StartAnimation("axechop");
-
-                //if (api.Side == EnumAppSide.Server)
-                //api.World.PlaySoundAt(new AssetLocation("sounds/block/chop1"), byEntity, null, true, 32f, 0.75f);
-                //else
                 SetParticleColourAndPosition(interactedBlock.GetRandomColor((ICoreClientAPI)api, blockSel.Position, BlockFacing.NORTH), blockSel.Position.ToVec3d());
 
                 handling = EnumHandHandling.Handled;
